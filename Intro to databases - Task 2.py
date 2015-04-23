e306b648d4d92a2b77ad3c5895b9068205931fcc
41213 <41213@PC140034.coll.lr.local>
2015-04-23 10:40:03 +0100

    Into to databases - Task 2 - Python school

---
+++ Intro to databases - Task 2.py
@@ -0,0 +1,35 @@
+#Michelle Yick
+#20/04/2015
+#Intro to databases - Python school - Task 2
+
+import sqlite3
+
+def create_table(db_name,table_name,sql):
+    with sqlite3.connect(db_name)as db:
+        cursor=db.cursor()
+        cursor.execute"select name from sqlite_master where name=?",(table_name,)) #detect whether or not the table already exists
+        result=cursor.fetchall()
+        keep_table=True
+        if len(result)==1:
+            response=input("The table {0} already exists, do you wish to recreate it (y/n):".format(table_name))
+            if response=="y":
+                keep_table=False
+                print("The {0} table will be recreated - all existing data will be lost".format(table_name))
+                cursor.execute("drop table if exists {0)".format(table_name))
+                db.commit()
+            else:
+                print("The existing table was kept")
+        else:
+            keep_table=False
+        if not keep_table:
+            cursor.execute(sql)
+            db.commit()
+
+if __name__=="__main__":
+    db_name="coffee_shop.db"
+    sql="""create table Product
+           (ProductID integer,
+           Name text,
+           Price real,
+           primary key(ProductID))"""
+    create_table(db_name,"Product" sql)
