5788ba8942302c841c31ba2bb7824dba24a13c9c
41213 <41213@PC140084.coll.lr.local>
2015-04-23 14:15:12 +0100

    Deleting items - Python school - Task 4

---
+++ Deleting items - Task 4.py
@@ -0,0 +1,16 @@
+#Michelle Yick
+#23/04/2015
+#Deleting data - Python school - Task 4
+
+import sqlite3
+
+def delete_product(data):
+    with sqlite3.connect("coffee_shop.db") as db:
+        cursor=db.cursor()
+        sql="delete from Product where Name=?"
+        cursor.execute(sql,data)
+        db.commit()
+
+if __name__=="__main__":
+    data=("Black Tea",)
+    delete_product(data)
